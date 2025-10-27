3c2696503501ed11d4ea6ae5479668f04e551283
GukGwonGae <gukgwongae@gmail.com>
2025-10-27 14:13:42 +0900

    feature: add cart_page with refactor

--- lib/view/cart_page/cart_list_view.dart
+++ lib/view/cart_page/cart_list_view.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project_3/view/cart_page/cart_page.dart';
 
 class CartListView extends StatelessWidget {
-  final List<CartEntity> cartItems;
+  final List<BookEntity> cartItems;
   final Function(int) onRemove;
-  final Function(int, int) onUpdateHowMany;
+  final Function(int, int) onUpdateCount;
+  final Function(int, bool) onItemCheck;
 
   const CartListView({
     super.key,
     required this.cartItems,
     required this.onRemove,
-    required this.onUpdateHowMany,
+    required this.onUpdateCount,
+    required this.onItemCheck,
   });
 
-  int totalPrice() {
-    return cartItems.fold(
-      0,
-      (total, item) => total + (item.price * item.howMany),
-    );
+  int selectedItemsPrice() {
+    return cartItems
+        .where((item) => item.isChecked)
+        .fold(0, (total, item) => total + (item.price * item.count));
   }
 
   @override
@@ -34,26 +35,58 @@ class CartListView extends StatelessWidget {
                 margin: EdgeInsets.only(bottom: 16),
                 elevation: 2,
                 child: Padding(
-                  padding: EdgeInsets.all(16),
+                  padding: EdgeInsets.symmetric(vertical: 8),
                   child: Row(
                     children: [
+                      // 체크 버튼
+                      CheckButton(
+                        isChecked: cartItem.isChecked,
+                        onTap: (bool? value) =>
+                            onItemCheck(index, value ?? false),
+                      ),
                       // 책 이미지
-                      Icon(Icons.book, size: 85),
-                      SizedBox(width: 16),
+                      cartItem.image != null && cartItem.image!.isEmpty
+                          ? Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.grey[300],
+                              ),
+                              width: 80,
+                              height: 120,
+                              child: Center(
+                                child: Text(
+                                  "No\nImage",
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            )
+                          : Image.network(
+                              cartItem.image!,
+                              width: 80,
+                              height: 120,
+                              fit: BoxFit.cover,
+                            ),
+                      SizedBox(width: 12),
                       Expanded(
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            // 책 이름
-                            Text(cartItem.name, maxLines: 1),
                             SizedBox(height: 8),
-                            // 책 가격
-                            Text('${cartItem.price.toStringAsFixed(0)}원'),
+                            // 책 이름
+                            Text(
+                              "도서명 : ${cartItem.title}",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              maxLines: 1,
+                            ),
+                            // 책 저자
+                            Text("저자 : ${cartItem.author}"),
                             SizedBox(height: 12),
+                            // 책 가격
+                            Text('가격 : ${cartItem.price.toStringAsFixed(0)}원'),
                             // 책 수량 버튼
-                            HowManyButton(
+                            CountButton(
                               index: index,
-                              onUpdateHowMany: onUpdateHowMany,
+                              onUpdateCount: onUpdateCount,
                               cartItem: cartItem,
                             ),
                           ],
@@ -78,23 +111,26 @@ class CartListView extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                // 총 금액
-                children: [
-                  Text('총 금액:'),
-                  Text('  ${totalPrice().toStringAsFixed(0)}원'),
-                ],
+              Text(
+                '결제 금액 : ${selectedItemsPrice().toStringAsFixed(0)}원',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
               // 결제 버튼
               TextButton(
-                onPressed: () {
-                  // 기능 미구현
-                },
+                onPressed: cartItems.any((item) => item.isChecked)
+                    ? () {
+                        // 기능 미구현
+                      }
+                    : null,
                 style: TextButton.styleFrom(
-                  backgroundColor: Colors.grey[600],
+                  backgroundColor: cartItems.any((item) => item.isChecked)
+                      ? Colors.grey[600]
+                      : Colors.grey[400],
                   foregroundColor: Colors.white,
                 ),
-                child: Text('결제하기'),
+                child: Text(
+                  "${cartItems.where((item) => item.isChecked).length}개 구매하기",
+                ),
               ),
             ],
           ),
@@ -112,7 +148,7 @@ class DeleteCartButton extends StatelessWidget {
   });
 
   final int index;
-  final Function(int p1) onRemove;
+  final Function(int index) onRemove;
 
   @override
   Widget build(BuildContext context) {
@@ -123,7 +159,7 @@ class DeleteCartButton extends StatelessWidget {
           builder: (BuildContext context) {
             return AlertDialog(
               title: Center(child: Text('진짜 안 사?')),
-              content: Text('진짜 진짜 진짜 안 사?'),
+              content: Text('진짜 진짜 진짜 진짜 진짜 진짜 진짜 진짜 안 사?'),
               actions: [
                 TextButton(
                   onPressed: () => Navigator.of(context).pop(),
@@ -146,17 +182,17 @@ class DeleteCartButton extends StatelessWidget {
   }
 }
 
-class HowManyButton extends StatelessWidget {
-  const HowManyButton({
+class CountButton extends StatelessWidget {
+  const CountButton({
     super.key,
     required this.index,
-    required this.onUpdateHowMany,
+    required this.onUpdateCount,
     required this.cartItem,
   });
 
   final int index;
-  final Function(int p1, int p2) onUpdateHowMany;
-  final CartEntity cartItem;
+  final Function(int index, int newCount) onUpdateCount;
+  final BookEntity cartItem;
 
   @override
   Widget build(BuildContext context) {
@@ -164,24 +200,36 @@ class HowManyButton extends StatelessWidget {
       children: [
         IconButton(
           onPressed: () {
-            onUpdateHowMany(index, cartItem.howMany - 1);
+            onUpdateCount(index, cartItem.count - 1);
           },
           icon: Icon(Icons.remove),
         ),
         Container(
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey[300]!),
+            border: Border.all(color: Colors.grey[500]!),
           ),
-          child: Text('${cartItem.howMany}'),
+          child: Text('${cartItem.count}'),
         ),
         IconButton(
           onPressed: () {
-            onUpdateHowMany(index, cartItem.howMany + 1);
+            onUpdateCount(index, cartItem.count + 1);
           },
           icon: Icon(Icons.add),
         ),
       ],
     );
+  }
+}
+
+class CheckButton extends StatelessWidget {
+  final bool isChecked;
+  final Function(bool?) onTap;
+
+  const CheckButton({super.key, required this.isChecked, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: isChecked, onChanged: onTap);
   }
 }
--- lib/view/cart_page/cart_page.dart
+++ lib/view/cart_page/cart_page.dart
@@ -2,18 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project_3/view/cart_page/cart_list_view.dart';
 import 'package:flutter_project_3/view/cart_page/empty_cart.dart';
 
-class CartEntity {
-  final String name;
-  final String? image; // 이거 어떻게 해야할지 모르겠음 사진임
+class BookEntity {
+  final String title;
+  final String author;
+  final String? description;
+  final String? image;
   final int price;
-  final int howMany;
+  final int count;
+  final bool isChecked;
 
-  CartEntity({
-    required this.name,
+  BookEntity({
+    required this.title,
+    required this.author,
+    this.description,
     this.image,
     required this.price,
-    required this.howMany,
+    required this.count,
+    this.isChecked = false,
   });
+
+  // 코드 간소화
+  BookEntity copyWith({
+    String? title,
+    String? author,
+    String? description,
+    String? image,
+    int? price,
+    int? count,
+    bool? isChecked,
+  }) {
+    return BookEntity(
+      title: title ?? this.title,
+      author: author ?? this.author,
+      description: description ?? this.description,
+      image: image ?? this.image,
+      price: price ?? this.price,
+      count: count ?? this.count,
+      isChecked: isChecked ?? this.isChecked,
+    );
+  }
 }
 
 class CartPage extends StatefulWidget {
@@ -24,46 +51,50 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  List<CartEntity> cart = [
+  List<BookEntity> cart = [
     // 예시임
-    CartEntity(name: "토맛 토마토", image: "", price: 99999, howMany: 1),
-    CartEntity(name: "토마토맛 토", image: "", price: 88888, howMany: 2),
-    CartEntity(name: "토마토맛 토", image: "", price: 88888, howMany: 2),
+    BookEntity(
+      title: "토맛 토마토",
+      author: "토마토맛 토",
+      price: 99999,
+      count: 1,
+      image: "",
+      isChecked: false,
+    ),
   ];
 
-  void removeCart(int index) {
+  void removeItem(int index) {
     setState(() {
       cart.removeAt(index);
     });
   }
 
-  // 책 수량 0 보다 작아지지 않음
-  void updateHowMany(int index, int newHowMany) {
-    int zeroHowMany = newHowMany < 0 ? 0 : newHowMany;
+  // 책 수량 1 보다 작아지지 않음
+  void updateCount(int index, int newCount) {
+    int minCount = newCount < 1 ? 1 : newCount;
     setState(() {
-      cart[index] = CartEntity(
-        name: cart[index].name,
-        image: cart[index].image,
-        price: cart[index].price,
-        howMany: zeroHowMany,
-      );
+      cart[index] = cart[index].copyWith(count: minCount);
+    });
+  }
+
+  // 아이템 체크박스
+  void ItemCheck(int index, bool isChecked) {
+    setState(() {
+      cart[index] = cart[index].copyWith(isChecked: isChecked);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back),
-        title: Text('장바구니'),
-        elevation: 1,
-      ),
+      appBar: AppBar(title: Text('장바구니'), elevation: 1),
       body: cart.isEmpty
           ? EmptyCart()
           : CartListView(
               cartItems: cart,
-              onRemove: removeCart,
-              onUpdateHowMany: updateHowMany,
+              onRemove: removeItem,
+              onUpdateCount: updateCount,
+              onItemCheck: ItemCheck,
             ),
     );
   }
