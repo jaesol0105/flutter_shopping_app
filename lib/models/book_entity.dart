643d2c08f4cd436037617e464a1c9b6ac019ecde
unknown <musou6413@naver.com>
2025-10-28 23:28:15 +0900

    fix : 장바구니 이미 담겨있는 상품을 담았을때 수량 추가하기

--- lib/models/book_entity.dart
+++ lib/models/book_entity.dart
@@ -12,13 +12,32 @@ class BookEntity {
     this.images,
     required this.price,
   });
+
+  /** 객체 비교를 위한 비교연산자와 해쉬코드 
+   * 뒤늦게 id를 추가하는 작업을 하기엔 체력이 고갈돼서 못하고 이렇게라도 하게되었음 ㅜ.ㅜ 흑흑 */
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is BookEntity &&
+        other.title == title &&
+        other.author == author &&
+        other.price == price &&
+        other.description == description;
+  }
+
+  @override
+  int get hashCode =>
+      title.hashCode ^
+      author.hashCode ^
+      price.hashCode ^
+      (description?.hashCode ?? 0);
 }
 
 // 책과 수량
 class CartItem {
   final BookEntity book;
-  final int count;
-  const CartItem({required this.book, required this.count});
+  int count;
+  CartItem({required this.book, required this.count});
 
   // copyWith 필요한 값만 덮어쓰기 용도로 사용. 그래서 optional parameter 사용.
   CartItem copyWith({BookEntity? book, int? count}) =>
--- lib/view/home_page/home_page.dart
+++ lib/view/home_page/home_page.dart
@@ -104,7 +104,14 @@ class _HomePageState extends State<HomePage> {
 
   // [장바구니에 상품 추가]
   void addBookToCartList(BookEntity book, int count) {
-    cartList.add(CartItem(book: book, count: count));
+    final existingIndex = cartList.indexWhere((item) => item.book == book);
+    if (existingIndex != -1) {
+      // 이미 존재할 경우 수량만 증가
+      cartList[existingIndex].count += count;
+    } else {
+      // 존재하지 않으면 새로 추가
+      cartList.add(CartItem(book: book, count: count));
+    }
   }
 
   // [장바구니에서 상품 제거]
